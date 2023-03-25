a8de981c3f69719e4b2e3b71278920e6b5141a82
AlexanderGridinNET <alexander_gridin_dotnet@outlook.com>
2023-03-25 14:57:45 +0300

    add service-order-form ui

--- CarWrapFilm/CarWrapFilm/Models/FilmWrappingKit.cs
+++ CarWrapFilm/CarWrapFilm/Models/FilmWrappingKit.cs
@@ -2,16 +2,19 @@
 
 public class FilmWrappingKit
 {
+    private readonly uint _id;
     private readonly string _name;
     private readonly uint _price;
     private readonly string[]? _composition;
 
+    public uint Id => _id;
     public string Name => _name;
     public uint Price => _price;
     public string[]? Composition => _composition;
 
-    public FilmWrappingKit(string name, uint price, string[]? composition = null)
+    public FilmWrappingKit(uint id, string name, uint price, string[]? composition = null)
     {
+        _id = id;
         _name = name;
         _price = price;
         _composition = composition;
--- CarWrapFilm/CarWrapFilm/wwwroot/js/site.js
+++ CarWrapFilm/CarWrapFilm/wwwroot/js/site.js
@@ -1,4 +1,51 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+"use strict";
+
+const selected = "text-primary";
+
+function getById(id) {
+    return document.getElementById(id);
+}
+function removeSelectedRow(table) {
+    for (const row of table.rows) {
+        if (row.className === selected) {
+            row.className = "";
+            return;
+        }
+    }
+}
+function setFormDataAndScroll(serviceInput, serviceIdInput, row) {
+    serviceInput.value = row.cells[0].textContent;
+    serviceIdInput.value = parseInt(row.id);
+
+    let form = getById("service-order-form");
+    let rect = form.getBoundingClientRect();
+    window.scrollTo(rect.x, rect.y);
+}
+
+document.addEventListener("DOMContentLoaded", e => {
+    e.preventDefault();
+
+    const serviceInput = getById("form-service-input");
+    const serviceIdInput = getById("form-service-id-input");
+    const tableKits = getById("table-kits");
+    const tableWorks = getById("table-works")
+    for (const row of tableKits.rows) {
+        row.onclick = function (e) {
+            e.preventDefault();
+            $(this).addClass(selected).siblings().removeClass(selected);
+            removeSelectedRow(tableWorks);
+            setFormDataAndScroll(serviceInput, serviceIdInput, this);
+        };
+    }
+    for (const row of tableWorks.rows) {
+        row.onclick = function (e) {
+            e.preventDefault();
+            $(this).addClass(selected).siblings().removeClass(selected);
+            removeSelectedRow(tableKits);
+            setFormDataAndScroll(serviceInput, serviceIdInput, this);
+        };
+    }
+});
