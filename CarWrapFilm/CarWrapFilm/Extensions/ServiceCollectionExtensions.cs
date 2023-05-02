f75678212ad338ed546ff791d2eb41f2eecb894f
AlexanderGridinNET <alexander_gridin_dotnet@outlook.com>
2023-05-02 21:29:01 +0300

    add service collection extensions

---
+++ CarWrapFilm/CarWrapFilm/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using CarWrapFilm.Models;
+using CarWrapFilm.Services;
+
+namespace CarWrapFilm.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTelegramDependency(this IServiceCollection @this, ConfigurationManager configuration, IWebHostEnvironment environment)
+    {
+        var token = configuration.GetSection("Telegram:BotToken").Value;
+        var users = new List<TelegramUser>
+        {
+            new(
+                configuration.GetSection("Telegram:Users:AlexGrid:Name").Value,
+                configuration.GetSection("Telegram:Users:AlexGrid:ChatId").Value.ToLong()
+            )
+        };
+        if (!environment.IsDevelopment())
+        {
+            users.AddRange(new TelegramUser[]
+            {
+                new(
+                    configuration.GetSection("Telegram:Users:SergDerg:Name").Value,
+                    configuration.GetSection("Telegram:Users:SergDerg:ChatId").Value.ToLong()
+                ),
+                new(
+                    configuration.GetSection("Telegram:Users:SergKisha:Name").Value,
+                    configuration.GetSection("Telegram:Users:SergKisha:ChatId").Value.ToLong()
+                )
+            });
+        }
+        @this.AddScoped<IMessageSender, TelegramBotMessageSender>(_ => new(new(token, users)));
+
+        return @this;
+    }
+}
--- CarWrapFilm/CarWrapFilm/Extensions/WebApplicationBuilderExtensions.cs
+++ CarWrapFilm/CarWrapFilm/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,7 +1,4 @@
-﻿using CarWrapFilm.Models;
-using CarWrapFilm.Services;
-
-namespace CarWrapFilm.Extensions;
+﻿namespace CarWrapFilm.Extensions;
 
 public static class WebApplicationBuilderExtensions
 {
@@ -9,27 +6,9 @@ public static class WebApplicationBuilderExtensions
     {
         var services = @this.Services;
         var configuration = @this.Configuration;
+        var environment = @this.Environment;
 
-        var token = configuration.GetSection("Telegram:BotToken").Value;
-        var users = new List<TelegramUser>
-        {
-            new TelegramUser(
-                configuration.GetSection("Telegram:Users:AlexGrid:Name").Value,
-                configuration.GetSection("Telegram:Users:AlexGrid:ChatId").Value.ToLong()
-            )/*,
-            new TelegramUser(
-                configuration.GetSection("Telegram:Users:SergDerg:Name").Value,
-                configuration.GetSection("Telegram:Users:SergDerg:ChatId").Value.ToLong()
-            ),
-            new TelegramUser(
-                configuration.GetSection("Telegram:Users:SergKisha:Name").Value,
-                configuration.GetSection("Telegram:Users:SergKisha:ChatId").Value.ToLong()
-            )*/
-        };
-        var telegramConfiguration = new TelegramConfiguration(token, users);
-
-        services.AddScoped<IMessageSender, TelegramBotMessageSender>(_ => new(telegramConfiguration));
-        services.AddRazorPages();
+        services.AddTelegramDependency(configuration, environment).AddRazorPages();
 
         return @this;
     }
